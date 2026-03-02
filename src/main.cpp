050e73c6113dcc626c2b67d155fd0cc55633c77d
pranavrajesh25 <pranavrajesh9@gmail.com>
2026-03-02 12:14:36 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,9 @@
 #define blue 1.93 //Value for a blue light with a red filter
 #define FULL_POWER 40.
 #define HALF_POWER 20.
+#define SERVO_INIT 500
+#define SERVO_FINAL 23858
+
 
 //Declare Motors, Encoders & CdS Cell
 FEHMotor left_motor(FEHMotor::Motor0,9.0);
@@ -18,12 +21,14 @@ FEHMotor right_motor(FEHMotor::Motor3,9.0);
 DigitalEncoder right_encoder(FEHIO::Pin8);
 DigitalEncoder left_encoder(FEHIO::Pin9);
 AnalogInputPin CdS_cell(FEHIO::Pin3);
+FEHServo servo_arm(FEHServo::Servo7);
 
 void start(); 
 void move_forward(int percent, float counts);
 void turn_right(int percent, int counts);
 void turn_left(int percent, int counts); 
 float transitions_count (float s);
+void compost_bin();
 
 void ERCMain()
 {
@@ -102,4 +107,21 @@ void turn_left(int percent, int counts) //Turn Left for a specified distance at 
 float transitions_count (float s)//Calculate the number of transitions the encoder needs to count to tranvel a specified distance (s) in inches
 {
     return ((s*N)/(2*PI*r));
+}
+
+//rotate the compost bin from 0 to 300 degrees, wait one second before rotating it back to it's original position
+void compost_bin(){
+
+    servo_arm.SetMin(SERVO_INIT); 
+    servo_arm.SetMax(SERVO_FINAL); 
+
+    int i=1; 
+    while (i==1) { 
+        
+        servo_arm.SetDegree(0); 
+        servo_arm.SetDegree(300);
+        Sleep(1.0); 
+        servo_arm.SetDegree(0);
+
+    }; 
 }
