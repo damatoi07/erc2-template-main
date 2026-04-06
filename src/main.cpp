ce1878ccae5f84008563e5e4b4b5ed6f57f79764
damatoi07 <damato.36@buckeyemail.osu.edu>
2026-04-06 12:05:27 -0400

    Full Code WIP

--- src/main.cpp
+++ src/main.cpp
@@ -11,42 +11,41 @@
 #define r 2.5/2 //Radius of Wheels
 #define red 0.29 //Value for a red light with a red filter
 #define blue 1.93 //Value for a blue light with a red filter
-#define FULL_POWER 50.
+#define FULL_POWER 40. 
+#define RAMP_POWER 65. 
 #define TURN_POWER 40.
 #define HALF_POWER 20.
-#define SERVO_INIT 500
-#define SERVO_FINAL 23858 
-#define turn_count_90 245 //Count input needed to make a 90 degree turn
-#define turn_count_45 100 //Count input needed to make a 45 degree turn
-#define UP 0 //Starting degree value for falling servo arm
-#define DOWN 90 //Starting degree value for falling servo arm
-#define HALF 45.0
-#define SERVO_MIN 500
-#define SERVO_MAX 23858 
+#define turn_count_90 230//Count input needed to make a 90 degree turn
+#define turn_count_45 105 //Count input needed to make a 45 degree turn
+#define UP 0 //Starting degree value for falling arm
+#define DOWN 90 //Starting degree value for falling arm
+// #define SERVO_MIN 500
+// #define SERVO_MAX 23858 
 #define DOWN_Percentage 20  //Falling arm percentage for going down
 #define UP_Percentage -30 //Falling arm percentage for going up
-#define DOWN_Percentage_Lever -15  //Lever arm percentage for going down
-#define UP_Percentage_Lever 50 //Lever arm percentage for going up
+#define DOWN_Percentage_Lever -30  //Lever arm percentage for going down
+#define UP_Percentage_Lever 45 //Lever arm percentage for going up
 #define RCS_WAIT_TIME_IN_SEC 0.35 //RCS Delay Time
-#define PULSE_TIME 0.17
-#define PULSE_POWER 25
+#define PULSE_TIME 0.17 //Time to pulse towards a direction to make corrections in position
+#define PULSE_POWER 25 //Power to the motors when pulsing towards a direction to make corrections in position
 #define PULSE_COUNT 10 //Turn count for heading check
-#define PLUS 0 // Orientation of AruCo Code
-#define MINUS 1 // Orientation of AruCo Code
+#define PLUS 0 //Orientation of AruCo Code
+#define MINUS 1 //Orientation of AruCo Code
 
 
-//Declare Motors, Encoders & CdS Cell
-FEHMotor left_motor(FEHMotor::Motor0,9.0);
-FEHMotor right_motor(FEHMotor::Motor3,9.0);
-DigitalEncoder right_encoder(FEHIO::Pin8);
-DigitalEncoder left_encoder(FEHIO::Pin9);
+//Declare Motors, Encoders & CdS Cell0
+FEHMotor left_motor(FEHMotor::Motor0,9.0); //IGWAN
+FEHMotor right_motor(FEHMotor::Motor3,9.0); //IGWAN
+DigitalEncoder right_encoder(FEHIO::Pin8); 
+DigitalEncoder left_encoder(FEHIO::Pin9); 
 AnalogInputPin CdS_cell(FEHIO::Pin14);
-FEHMotor falling_arm_motor(FEHMotor::Motor1, 9.0);
-FEHMotor lever_arm_motor (FEHMotor::Motor2, 9.0);
+FEHMotor falling_arm_motor(FEHMotor::Motor1, 9.0); //TT Motor
+FEHMotor lever_arm_motor (FEHMotor::Motor2, 9.0); //TT Motor
 
 
 int start(); 
 void move_forward(int percent, float counts);
+void up_ramp (int percent, float counts);
 void turn_right(int percent, int counts);
 void turn_left(int percent, int counts); 
 float transitions_count (float s);
@@ -54,44 +53,71 @@ void compost_bin();
 void turn_to_humidifier();
 void move_falling_arm (int percent);
 void lever_arm(int position);
+void lever_arm_start();
 void flip_fertilizer();
 void RCS_heading_check ();
 void check_x(float x_coordinate, int orientation);
 void check_y(float x_coordinate, int orientation);
 void pulse_forward(int percent, float seconds);
-void pulse_counterclockwise(int percent, float seconds);
 void RCS_heading_check (int heading_angle);
 
 void ERCMain()
 {
-    //Apple Bucket to Bottom of Ramp
+int initiate=start();
+    if (initiate==1){
+
+    //Apple Bucket to Top of Ramp
+    lever_arm_start();
     move_forward(-FULL_POWER,(transitions_count(4)));
     move_forward(FULL_POWER,(transitions_count(18)));
     turn_left(TURN_POWER,turn_count_45);
-    move_forward(FULL_POWER,(transitions_count(6.75)));
+    move_forward(FULL_POWER,(transitions_count(9)));
     lever_arm(UP);
-    move_forward(-FULL_POWER,(transitions_count(6)));
-    turn_left(TURN_POWER,(turn_count_90+turn_count_90));
-    move_forward(FULL_POWER,(transitions_count(25)));
-    turn_left(TURN_POWER,(turn_count_90+turn_count_45));
+    move_forward(-FULL_POWER,(transitions_count(9)));
+    turn_right(TURN_POWER,turn_count_45);
+    move_forward(-FULL_POWER,(transitions_count(8)));
+    turn_left(TURN_POWER,turn_count_45);
+    move_forward(-FULL_POWER,(transitions_count(10)));
+    move_forward(FULL_POWER,(transitions_count(3)));
+    turn_right(TURN_POWER,turn_count_90);
+    move_forward(RAMP_POWER,(transitions_count(45)));
 
-    //Bottom Of Ramp to Crate
-    move_forward(FULL_POWER,(transitions_count(27)));
-    turn_left(TURN_POWER,turn_count_90);
+    //Wall Alignment
+    move_forward(-FULL_POWER,(transitions_count(10)));
+    turn_left(TURN_POWER,(turn_count_45/2));
+    move_forward(FULL_POWER,(transitions_count(8)));
+    turn_left(TURN_POWER,(turn_count_45+(turn_count_45/2)));
+    move_forward(-FULL_POWER,(transitions_count(10)));
+
+    //Wall to Crate
     move_forward(FULL_POWER,(transitions_count(6)));
     turn_right(TURN_POWER,turn_count_90);
-    move_forward(FULL_POWER,(transitions_count(17.5)));
+    move_forward(FULL_POWER,(transitions_count(20)));
     lever_arm(DOWN);
-    move_forward(-FULL_POWER,(transitions_count(17.5)));
-
+    
     //Crate to Levers
-    turn_left(TURN_POWER,turn_count_90);
-    move_forward(-FULL_POWER,(transitions_count(4)));
+    move_forward(-FULL_POWER,(transitions_count(17.25)));
+    lever_arm(UP);
     turn_left(TURN_POWER,turn_count_45);
-    move_forward(FULL_POWER,(transitions_count(12)));
+    move_forward(FULL_POWER,(transitions_count(17)));
+    Sleep(1.0); 
+    lever_arm(DOWN);
+    move_forward(-FULL_POWER,(transitions_count(5))); 
+    move_forward(FULL_POWER,(transitions_count(5))); 
+    lever_arm(UP);
 
-
-
+    //Levers to humidifier light
+    move_forward(-FULL_POWER,(transitions_count(17)));
+    turn_left(TURN_POWER,turn_count_45);
+    move_falling_arm(DOWN);
+    move_forward(FULL_POWER,(transitions_count(8.5)));
+    Sleep(3.0); //Check Humidifier Light Position
+    move_falling_arm(UP);
+    turn_to_humidifier();
+    move_falling_arm(DOWN);
+    move_forward(-FULL_POWER,(transitions_count(18))); 
+    Sleep(3.0);
+    }
 }
 
 int start ()//Go after start light is detected to be ON or after 30 seconds
@@ -118,7 +144,29 @@ void move_forward(int percent, float counts) //Drive Forward for a specified dis
     left_encoder.ResetCounts();
 
     //Set both motors to desired percent
-    right_motor.SetPercent(percent+2);
+    right_motor.SetPercent(percent);
+    left_motor.SetPercent(percent);
+
+
+    //While the average of the left and right encoder is less than counts,
+    //keep running motors
+    while((left_encoder.Counts() + right_encoder.Counts()) / 2. < counts);
+
+        LCD.WriteLine(left_encoder.Counts());
+        LCD.WriteLine(right_encoder.Counts());
+
+    //Turn off motors
+    right_motor.Stop();
+    left_motor.Stop();
+};
+void up_ramp (int percent, float counts) //Drive Forward for a specified distance at a specified speed using encoders
+{
+    //Reset encoder counts
+    right_encoder.ResetCounts();
+    left_encoder.ResetCounts();
+
+    //Set both motors to desired percent
+    right_motor.SetPercent(percent+15);
     left_motor.SetPercent(percent);
 
 
@@ -168,7 +216,7 @@ void turn_left(int percent, int counts) //Turn Left for a specified distance at 
     right_motor.Stop();
     left_motor.Stop();
 };
-float transitions_count (float s)//Calculate the number of transitions the encoder needs to count to tranvel a specified distance (s) in inches
+float transitions_count (float s)//Calculate the number of transitions the encoder needs to count to travel a specified distance (s) in inches
 {
     return ((s*N)/(2*PI*r));
 };
@@ -224,10 +272,16 @@ void lever_arm(int position){
         lever_arm_motor.Stop();
         LCD.WriteLine("DOWN"); 
         lever_arm_motor.SetPercent(DOWN_Percentage_Lever);
-        Sleep (0.35);
+        Sleep (2.0);
         lever_arm_motor.Stop();
         break;
     }
+};
+void lever_arm_start(){
+        LCD.WriteLine("DOWN"); 
+        lever_arm_motor.SetPercent(-10);
+        Sleep (.45);
+        lever_arm_motor.Stop();
 };
 void flip_correct_lever(){
     int lever = RCS.GetLever();
@@ -273,9 +327,9 @@ void RCS_heading_check (int heading_angle){
     RCS.RequestPosition(false);
     Sleep(0.1);
     RCSPose* pose = RCS.Position();
+    int i = 0;
 
-    for (int i = 0; i < 10; i++) {
-        if(pose->heading != heading_angle && (pose->heading < heading_angle - 1 || pose->heading > heading_angle + 1))
+        if(pose->heading != heading_angle && (((heading_angle + 1) < pose->heading < (heading_angle - 1)) && i !=5))
         {
             if(pose->heading > heading_angle + 1)
             {
@@ -288,7 +342,6 @@ void RCS_heading_check (int heading_angle){
             Sleep(RCS_WAIT_TIME_IN_SEC);
 
             pose = RCS.RequestPosition();
-        }
     }
 };
 void check_x(float x_coordinate, int orientation){
@@ -299,13 +352,14 @@ void check_x(float x_coordinate, int orientation){
         power = -PULSE_POWER;
     }
 
+    int i=0;
+
     RCSPose* pose = RCS.RequestPosition();
 
     // Check if receiving proper RCS coordinates and whether the robot is within an acceptable range
-    for (int i = 0; i < 10; i++) {
-        if(pose->x != -1 && (pose->x < x_coordinate - 1 || pose->x > x_coordinate + 1))
+        if(pose->x != -1 && ((x_coordinate + 1) < pose->x < (x_coordinate - 1)) && i !=5)
         {
-            if(pose->x > x_coordinate + 1)
+            if(pose->x > x_coordinate + 1) 
             {
                 // Pulse the motors for a short duration in the correct direction
                 pulse_forward(-power, PULSE_TIME);
@@ -318,8 +372,9 @@ void check_x(float x_coordinate, int orientation){
             Sleep(RCS_WAIT_TIME_IN_SEC);
 
             pose = RCS.RequestPosition();
+
+            i++;
         }
-    }
 };
 void check_y(float y_coordinate, int orientation)
 {
@@ -329,11 +384,13 @@ void check_y(float y_coordinate, int orientation)
         power = -PULSE_POWER;
     }
 
+    int i=0;
+
+
     RCSPose* pose = RCS.RequestPosition();
 
     // Check if receiving proper RCS coordinates and whether the robot is within an acceptable range
-    for (int i = 0; i < 10; i++) {
-        while(pose->y != -1 && (pose->y < y_coordinate - 1 || pose->y > y_coordinate + 1))
+        while(pose->y != -1 && ((y_coordinate + 1) < pose->y < (y_coordinate - 1)) && i !=5)
         {
             if(pose->y > (y_coordinate + 1))
             {
@@ -348,7 +405,8 @@ void check_y(float y_coordinate, int orientation)
             Sleep(RCS_WAIT_TIME_IN_SEC);
             
             pose = RCS.RequestPosition();
-        }
+
+            i++;
     }   
 };
 void pulse_forward(int percent, float seconds) 
@@ -364,16 +422,3 @@ void pulse_forward(int percent, float seconds)
     right_motor.Stop();
     left_motor.Stop();
 }
-void pulse_counterclockwise(int percent, float seconds) 
-{
-    // Set both motors to desired percent
-    right_motor.SetPercent(percent);
-    left_motor.SetPercent(-percent);
-
-    // Wait for the correct number of seconds
-    Sleep(seconds);
-
-    // Turn off motors
-    right_motor.Stop();
-    left_motor.Stop();
-};
