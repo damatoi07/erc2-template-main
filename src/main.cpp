5114a2af043adb2ac728d0fa8cc4d571d9e9ebc4
damatoi07 <damato.36@buckeyemail.osu.edu>
2026-04-15 15:12:54 -0400

    Full Course Code V6

--- src/main.cpp
+++ src/main.cpp
@@ -63,37 +63,29 @@ void check_x(float x_coordinate, int orientation);
 void check_y(float x_coordinate, int orientation);
 void pulse_forward(int percent, float seconds);
 void RCS_heading_check (float heading_angle);
+void record_positions();
 
 void ERCMain()
 {
     //initialize the RCS
     RCS.InitializeTouchMenu("1130D7LFS"); 
 
+    record_positions();
+
     //Store Coordinate values to RCS
-    int touch_x,touch_y;
-    float light_x, light_y, B_x, B_y, C_x, C_y, D_x, D_y;
-    float light_heading, B_heading, C_heading, D_heading;
+    float light_x, light_y; //Position of CdS cell over humidifier light
+    float levers_x, levers_y; //Position of Robot at center of levers?
+    float C_x, C_y, D_x, D_y;
 
-    LCD.WriteLine("RCS & Data Logging");
-    LCD.WriteLine("Press Screen To Start");
-    while(!LCD.Touch(&touch_x,&touch_y));
-    while(LCD.Touch(&touch_x,&touch_y));
+    FEHFile* fptr = SD.FOpen("RCS_POSITIONS.txt", "r");
 
-    FEHFile* fptr = SD.FOpen("RCS_TEST.txt", "r");
-    
     SD.FScanf(fptr, "%f%f", &light_x, &light_y);
-    // SD.FScanf(fptr, "%f%f", &B_x, &B_y);
+    SD.FScanf(fptr, "%f%f", &levers_x, &levers_y);
     // SD.FScanf(fptr, "%f%f", &C_x, &C_y);
     // SD.FScanf(fptr, "%f%f", &D_x, &D_y);
 
-    SD.FPrintf(fptr, "Light: %.2f, %.2f\n", light_x, light_y);
-    SD.FPrintf(fptr, "B: %.2f, %.2f\n", B_x, B_y);
-    SD.FPrintf(fptr, "C: %.2f, %.2f\n", C_x, C_y);
-    SD.FPrintf(fptr, "D: %.2f, %.2f\n", D_x, D_y);
-
     SD.FClose(fptr);
    
-
     WaitForFinalAction();
 
     int initiate=start();
@@ -132,14 +124,14 @@ void ERCMain()
     lever_arm(DOWN);
     
     //Crate to Humidifier Light
-    // check_x(light_x, BACKWARDS);
-    move_forward(-FULL_POWER,(transitions_count(15.5)));
+    check_x(light_x, BACKWARDS);
+    // move_forward(-FULL_POWER,(transitions_count(15.5)));
     turn_left(TURN_POWER,turn_count_90);
     move_falling_arm(DOWN);
     lever_arm(UP);
     RCS_heading_check(180.0);
-    Sleep(3.0);
-    move_forward(FULL_POWER,(transitions_count(10)));
+    // move_forward(FULL_POWER,(transitions_count(10)));
+    check_y(light_y,FORWARDS);
     Sleep(3.0); //Check Humidifier Light Position 
     move_forward(FULL_POWER,(transitions_count(12)));
     Sleep(5.0); //Check Humidifier Light Position 
@@ -176,6 +168,10 @@ void ERCMain()
     // move_forward(-FULL_POWER,(transitions_count(5)));
     // Sleep(3.0); //Check compost position
     // compost_bin();
+
+    LCD.WriteRC("Requests left: ", 0, 0);
+    LCD.WriteRC((int)RCS.RequestsRemaining(), 0, 15);
+    
     }
     
    
@@ -511,3 +507,60 @@ void pulse_forward(int percent, float seconds)
     right_motor.Stop();
     left_motor.Stop();
 }
+void record_positions(){
+    // Declare variables for touchscreen functionality.
+    int touch_x, touch_y;
+    // Loop n through points to record necessary positions.
+    int n;
+    char points[4] = {'A', 'B', 'C', 'D'};
+
+    // Open SD file for writing
+    FEHFile *fptr = SD.FOpen("test.txt", "w");
+
+    // Wait for touchscreen to be pressed and released
+    LCD.WriteLine("Press Screen to Start");
+    while (!LCD.Touch(&touch_x, &touch_y));
+    while (LCD.Touch(&touch_x, &touch_y));
+
+    // Clear screen
+    LCD.Clear();
+
+    // Step through each path point (A,B,C,D) to record position
+    for (n = 0; n <= 1; n++)
+    {
+        LCD.Clear();
+        LCD.WriteRC("Touch to set point ", 0, 0);
+        LCD.WriteRC(points[n], 0, 20);
+
+        RCSPose *pose;
+
+        // Loop until touchscreen is pressed, continuously requesting 
+        // RCS position data and writing it to the LCD screen
+        while (!LCD.Touch(&touch_x, &touch_y))
+        {
+            // Request position data from RCS
+            pose = RCS.RequestPosition();
+
+            // Clear previous position data from LCD
+            LCD.SetFontColor(BLACK);
+            LCD.FillRectangle(130, 20, 100, 60);
+
+            LCD.SetFontColor(WHITE);
+            LCD.WriteRC("X Position:", 2, 0);
+            LCD.WriteRC("Y Position:", 3, 0);
+
+            // Write the RCS data to the LCD screen
+            LCD.WriteRC(pose->x, 2, 12);
+            LCD.WriteRC(pose->y, 3, 12);
+        }
+
+        // Print RCS data for this path point to file
+        SD.FPrintf(fptr, "%f %f\n", pose->x, pose->y);
+
+        // Wait for touchscreen to be released
+        while (!LCD.Touch(&touch_x, &touch_y));
+    }
+
+    // Close SD file
+    SD.FClose(fptr);
+};
